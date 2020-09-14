36f56d202ed166740c6a468f33b4265547599a2d
SamKlep <shamdemann23@gmail.com>
2020-09-14 12:13:00 -0500

    Added app provider and connected to appbar.

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AppContext } from "./AppProvider";
 
 const Logo = styled.div`
   font-size: 1.5em;
@@ -20,12 +21,23 @@ const ControlButtonElem = styled.div`
     `}
 `;
 
-function toProperCase(lower){
-    return lower.charAt(0).toUpperCase() + lower.substr(1);
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
 function ControlButton({ name, active }) {
-  return <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>;
+  return (
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem 
+        active={page === name}
+        onClick={() => setPage(name)}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
+  );
 }
 
 export default function () {
