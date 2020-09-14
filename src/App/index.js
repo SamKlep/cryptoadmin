32f110aab1e85a8437451a4dce6dc4d95f22907d
SamKlep <shamdemann23@gmail.com>
2020-09-14 10:06:48 -0500

    Added styled components package.

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import WelcomeMessage from "./WelcomeMessage";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <WelcomeMessage />
     </div>
   );
 }
