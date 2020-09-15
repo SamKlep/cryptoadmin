53fb233a26137fc3f296c42b6a912c32ad150518
SamKlep <shamdemann23@gmail.com>
2020-09-15 08:51:07 -0500

    Add loading content.

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
-
 import AppLayout from "./AppLayout";
 import AppBar from "./AppBar";
 import { AppProvider } from "./AppProvider";
 import Settings from "../Settings";
+import Content from "../shared/Content";
 
 function App() {
   return (
     <AppLayout>
       <AppProvider>
         <AppBar />
+        <Content>
         <Settings />
+        </Content>
       </AppProvider>
     </AppLayout>
   );
